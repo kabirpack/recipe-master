96f80e0eaeb744a5ec07591dd8e994ae9ec6570f
kabirpack <kabir3rd@gmail.com>
2022-02-07 18:59:38 +0530

    binding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-master';
+  loadedFeature : string = 'recipe';  //default
+
+  loadFeature(feature : string){
+    this.loadedFeature = feature;
+  }
 }
