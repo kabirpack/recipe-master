21a6f84bbb7ae827df628ac839f9f08f6d45fd47
SyedKabir <gs1-syedkabirs@corp.expeditors.com>
2022-02-07 18:59:38 +0530

    component binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,15 +8,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes : Recipe[] = [
+  recipeList : Recipe[] = [
 
-    new Recipe('Test Recipe', 'Test Description', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg'),
-    new Recipe('Test Recipe', 'Test Description', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg')
+    new Recipe('Test Recipe1', 'Test Description1', 'https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg'),
+    new Recipe('Test Recipe2', 'Test Description2', 'https://static.onecms.io/wp-content/uploads/sites/9/2021/02/12/roast-chicken-with-chile-basil-vinaigrette-charred-broccoli-potatoes-FT-RECIPE0321.jpg'),
+    new Recipe('Test Recipe3', 'Test Description3', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
 
   ];
+
+  @Output() sendRecipeDetails = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  emitRecipeDetails(index : number){
+    this.sendRecipeDetails.emit(this.recipeList[index]);
+  }
 }
